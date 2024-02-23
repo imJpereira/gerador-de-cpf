8fa3cc0b74ce33bb584f9abad95b89daccede3e1
João <jps.pereira0805@gmail.com>
2024-02-23 09:23:37 -0300

    webpack configs, new README content and the algorithm that validate the CPF"

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# gerador-de-cpf
-esse é um programa que cria e valida CPFs
+# Gerador de CPFs Válidos
+Este é um programa simples em JavaScript que gera CPFs válidos de forma aleatória. O objetivo é fornecer uma ferramenta útil para testes e desenvolvimento, sem a necessidade de usar informações reais.
+
+## Como Funciona
+
+O programa utiliza um algoritmo para gerar CPFs válidos de acordo com as regras estabelecidas pela Receita Federal do Brasil.
+
+## Notas Importantes
+
+
+* Os CPFs gerados são válidos de acordo com o algoritmo utilizado, mas não representam pessoas reais.
+
+* Este programa não tem a intenção de incentivar atividades ilegais ou prejudiciais.
+
+* Utilize os CPFs gerados de forma ética e responsável, respeitando todas as leis e regulamentos locais.
---
+++ src/modules/ValidaCpf.js
@@ -0,0 +1,65 @@
+class ValidaCpf {
+    constructor(cpf) {
+        Object.defineProperty(this, "cleanCPF", {
+            value: cpf.replace(/\D+/g, ''),
+            enumerable: false,
+            writable: false,
+            configurable: false
+        })
+    }
+
+    validateCpf() {
+
+        if (this.cleanCPF.length !== 11) return false;
+        if (typeof this.cleanCPF !== "string") return false;
+        if (this.isSequential()) return false;
+        
+        this.createBaseArray();
+        this.calculation();
+        
+        return this.cleanCPF === this.baseArray.join('');
+    }
+
+    isSequential() {
+        const sequence = this.cleanCPF[0].repeat(11);
+        return sequence === this.cleanCPF;
+    }
+
+    createBaseArray() {
+        this.baseArray = this.cleanCPF.split('');
+        this.baseArray.splice(-2);
+    }
+
+    calculation() {
+        for(let i = 11; i <= 12; i++) this.calculateDigit(i);
+    }
+
+    calculateDigit(i) {
+        const multiplied = this.multiply(i);
+        const added = this.sum(multiplied);
+        const digit = this.getDigit(added);
+        this.baseArray.push(String(digit));
+    }
+
+    multiply(i) {
+        return this.baseArray.map(digit => {
+            i--
+            return i * Number(digit);
+        })
+    }
+
+    sum = multipliedArray => multipliedArray.reduce((ac, digit) => ac += digit, 0)
+
+    getDigit(number) {
+        const final = 11 - (number % 11);
+        return final >= 10 ? 0 : final;
+    }   
+}
+
+const user1 = new Cpf("070.987.720-03");
+
+if (user1.validateCpf()) {
+    console.log("CPF válido");
+} else {
+    console.log("CPF inválido");
+}
